51831a5755d71a24747b484e9841638db73e5cf3
João Luiz <jl.joaoscherer@gmail.com>
2022-10-10 19:25:06 -0300

    save

--- my-app/src/components/clientRegistration/index.js
+++ my-app/src/components/clientRegistration/index.js
@@ -6,6 +6,7 @@ import { DivCapsule, InputForText, InputForEmail, InputForPassWord, SelectedForS
   SpanForLink} from './style';
 
 const ClientRegistration = () => {
+<<<<<<< HEAD
   const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
   const [userId, setUserId] = useState('');
@@ -58,12 +59,26 @@ const ClientRegistration = () => {
     });
   }
 
+=======
+    const [firstName, setFirstName] = useState('');
+    const [email, setEmail] = useState('');
+    const dispatch = useDispatch();
+
+    function registerName() {
+        dispatch({
+          type: 'NEW_FIRST_NAME_REGISTER',
+          firstName: firstName
+        });
+      }
+    
+>>>>>>> c843181 (save)
   // método para verificar se nome já existe?
   function handleFirstName(e) {
     e.preventDefault();
     setFirstName(e.target.value);
   }
 
+<<<<<<< HEAD
   function handlePassWord(e) {
     e.preventDefault();
     setPassWord(e.target.value);
@@ -205,6 +220,31 @@ const ClientRegistration = () => {
     <FormForClient>
     
       <InputForText
+=======
+      function handleEmail(e) {
+        e.preventDefault();
+        setEmail(e.target.value);
+        fetch("http://whm.joao1866.c41.integrator.host:9206/usuario?email=" + this.email)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              exists: true
+            });
+          },
+          (error) => {
+            log.error(error)
+            this.setState({
+              isLoaded: true
+            });
+          }
+        )
+      }
+
+    return(
+        <InputForText 
+>>>>>>> c843181 (save)
         id='inputName'
         type="text"
         name="firstName"
@@ -212,7 +252,20 @@ const ClientRegistration = () => {
         onBlur={registerName}
         placeholder="Nome"
         maxLength="100"
+<<<<<<< HEAD
       />
+=======
+        />,
+        <InputForText 
+        id='inputEmail'
+        type="text"
+        name="inputEmail"
+        onChange={handleEmail}
+        onBlur={registerEmail}
+        placeholder="Email"
+        maxLength="100"
+        />
+>>>>>>> c843181 (save)
 
       <InputForEmail
         id='inputEmail'
