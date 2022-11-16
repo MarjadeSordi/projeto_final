c32e43ba28fc355c59d0324b1093d0da60334616
Marja de Sordi <marjadesordi@gmail.com>
2022-11-16 11:50:42 -0300

    Rotes e login

---
+++ my-app/src/routes/clientDashboardWithoutEmail/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ServicesWithoutLogin from '../../components/servicesWithoutLogin';
+
+const ClientDashboardWithoutEmail = () =>{
+    return(
+        <>
+        <ServicesWithoutLogin /></>
+    )
+}
+
+export default ClientDashboardWithoutEmail; 
